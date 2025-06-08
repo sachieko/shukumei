11fbe4804d0190376b8b785f21f227326cea84c6
Chelsea Sachieko <arashikabane@gmail.com>
2025-06-08 14:53:07 -0600

    Use server nickname if it exists in prediction replies

--- src/helpers/fetchUtils.ts
+++ src/helpers/fetchUtils.ts
@@ -1,6 +1,6 @@
 import { Interaction, CacheType, CommandInteraction } from "discord.js";
 
-export const fetchNickname = async (interaction: Interaction<CacheType> | CommandInteraction) => {
+export const fetchNickname = async (interaction: Interaction<CacheType> | CommandInteraction, userId?: string) => {
   const member = await interaction.guild?.members.cache.get(
     interaction.user.id
   );
