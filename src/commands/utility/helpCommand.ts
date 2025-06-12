b794a4b102293780aaec5b633e1783ffd4ace870
Chelsea Sachieko <dev@yuseiko.org>
2025-06-12 13:34:29 -0600

    Add dice emoji back to help message

--- src/commands/utility/helpCommand.ts
+++ src/commands/utility/helpCommand.ts
@@ -34,8 +34,8 @@ Enter the ring and skill for your approach determined with your GM. These are th
 If you have any assistance, you will have to enter the number of characters giving unskilled or skilled assist separately. If you spent a voidpent, select True for voidpoint. You may also enter the TN if you would like it displayed, as well as give the roll a label such as "Saburo's Tea Ceremony Check". To remove an optional category you did not mean to use, you can backspace to remove it.
 
 **Choosing kept dice, dice to reroll, or dice to modify:**
-The dice are displayed in a specific order, which you use to choose kept dice. Using the following dice roll: :blacko::blackexpst: :whitesst:
-You can keep 2 dice since the used Ring was 2. If you wanted to keep :blackexpst: :whitesst: then you need to keep the 2nd and 3rd die. To do this, click Keep under the roll and then enter "2 3" or "2,3" and you will keep the second and third dice. Using this method you can keep any number of dice up to the limit allowed for your roll based on Ring dice, assists, and void points spent. If you try to keep more than you are allowed, they are kept in order from left to right until you can't keep dice anymore.
+The dice are displayed in a specific order, which you use to choose kept dice. Using the following dice roll: ${DISCORD_DIE_EMOJI.D6[3]}${DISCORD_DIE_EMOJI.D6[6]}${DISCORD_DIE_EMOJI.D12[6]}
+You can keep 2 dice since the used Ring was 2. If you wanted to keep ${DISCORD_DIE_EMOJI.D6[6]}${DISCORD_DIE_EMOJI.D12[6]} then you need to keep the 2nd and 3rd die. To do this, click Keep under the roll and then enter "2 3" or "2,3" and you will keep the second and third dice. Using this method you can keep any number of dice up to the limit allowed for your roll based on Ring dice, assists, and void points spent. If you try to keep more than you are allowed, they are kept in order from left to right until you can't keep dice anymore.
 
 **Explosive Rerolls:**
 When you keep an explosive die, a new rolled die will be added to the original dice. Ex: if you keep an explosive in the example above, there will be 4 rolled dice.
